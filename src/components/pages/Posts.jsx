b35e5888b1f85b8d16c6a2bef169417967e57031
Adrian Rojo <rojoadrian16@gmail.com>
2022-10-07 15:53:27 -0500

    Created inital Posts page

--- src/components/pages/Posts.jsx
+++ src/components/pages/Posts.jsx
@@ -1,7 +1,43 @@
-export default function Posts() {
-	return (
-		<div>
-			hello from Posts
-		</div>
-	)
+import { useState } from "react"
+import axios from "axios"
+
+export default function posts(){
+	
+    const [posts, setPosts] = useState([])
+    const [errorMessage, setErrorMessage] = useState("")
+    
+
+
+    useEffect(() => {
+        const getPosts = async () => {
+            try{
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/posts`)
+                setPosts(response.data)
+            }catch(err){
+                setErrorMessage(err.message)
+
+            }
+        }
+        getPosts()
+},[])
+
+const renderPosts = posts.map((post) => {
+    return (
+        <div key={post.id}>
+            <img src={post.image_url} alt={post.title}/>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+            <p>{post.user_Id}</p>
+            <p>{post.comment}</p>
+            <p>{post.likes}</p>
+        </div>
+    )
+
+})
+    return(
+        <div>
+            <h1>Posts</h1>
+            {renderPosts}
+        </div>
+    )
 }
