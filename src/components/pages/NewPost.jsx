ee762bfd35f246cc043bd54770b89b85308e8869
Adrian Rojo <rojoadrian16@gmail.com>
2022-10-07 17:09:43 -0500

    saved files

--- src/components/pages/NewPost.jsx
+++ src/components/pages/NewPost.jsx
@@ -5,7 +5,7 @@ export default function NewPost(){
             <form>   
                 <label htmlFor="content">Content</label>
                 <input type="text" name="content" id="content"/>
-
+                {/* <input hidden type="text" name="user" id="user"/> */}
                     {/* for when we add image upload functionality */}
                 {/* <label htmlFor="image_url">Image URL</label> */}
                 {/* <input type="file" name="image_url" id="image_url"/> */}
