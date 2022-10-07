2beb8b180d5b8a817b6c4e8eadde98583e291f89
Adrian Rojo <rojoadrian16@gmail.com>
2022-10-07 16:57:00 -0500

    added bio submit function to the profile page

--- src/components/pages/Profile.jsx
+++ src/components/pages/Profile.jsx
@@ -36,6 +36,29 @@ export default function Profile({ currentUser, handleLogout }) {
 		}
 		fetchData()
 	},[])
+	const handleBioSubmit = async e => {
+		e.preventDefault()
+		try {
+			// get the token from local storage
+			const token = localStorage.getItem('jwt')
+			// make the auth headers
+			const options = {
+				headers: {
+					'Authorization': token
+				}
+			}
+			// hit the auth locked endpoint
+			const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/profile`, {bio: bio}, options)
+			// await axios.post(url, requestBody (form data), options
+			setMsg(response.data.msg)
+		} catch (err) {
+			// if the error is a 401 -- that means that auth failed
+			console.warn(err)
+			if (err.response) {
+				if (err.response.status === 401) {
+					handleLogout()
+				}
+			}
 	return (
 		<div>
 			<h1>Hello, {currentUser.name}</h1>
@@ -45,6 +68,17 @@ export default function Profile({ currentUser, handleLogout }) {
 			<h2>Here is the secret message that is only availible to users of User App:</h2>
 
 			<h3>{msg}</h3>
+			<form onSubmit={handleBioSubmit}>
+				<label htmlFor='bio'>Bio:</label>
+				<input
+					type="text"
+					name="bio"
+					value={bio}
+					onChange={e => setBio(e.target.value)}
+				/>
+				<button type="submit">Submit</button>
+			</form>
+			
 		</div>
 	)
-}
+}}}
