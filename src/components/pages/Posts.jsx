1e11564d20491daa7ffc037e8add2f6b063eff53
Adrian Rojo <rojoadrian16@gmail.com>
2022-10-08 15:58:57 -0500

    added current user id to posts

--- src/components/pages/Posts.jsx
+++ src/components/pages/Posts.jsx
@@ -1,17 +1,23 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
+import { dblClick } from "@testing-library/user-event/dist/click"
 
-export default function Posts(){
+export default function Posts({ currentUser, setCurrentUser }){
 	
     const [posts, setPosts] = useState([])
     const [errorMessage, setErrorMessage] = useState("")
     const [content, setContent] = useState("")
 
-
+   
     useEffect(() => {
         const getPosts = async () => {
             try{
-                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts`)
+                const options = {
+                    headers: {
+                        'userid': currentUser.id
+                    }
+                }
+                const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts`, options)
                 setPosts(response.data)
             }catch(err){
                 setErrorMessage(err.message)
@@ -23,22 +29,31 @@ export default function Posts(){
 
 
 
+// const findUserById = (id) => {
+//     const user = db.users.find({'_id': id})
+//     return user.username
+// }
+
+
 const renderPosts = posts.map((post, idx) => {
     return (
         <div key={`key-${idx}`}>
-            <img src={post.photo} alt={post._id}/>
+            {/* <img src={post.photo} alt={post._id}/> */}
             <p>{post.content}</p>
-            <p>{post.username}</p>
             {/* need to map an array of comments and hide it on Posts route */}
             {/* <p>{post.comment}</p> */}
             {/* changed this to '.length' to show number of likes */}
             <p>{post.likes.length} likes</p>
+            
+            {/* <p>{findUserById(post.user)}</p> */}
+            
         </div>
     )
 
 })
     return(
         <div>
+        
             <h1>Posts</h1>
             {renderPosts}
         </div>
