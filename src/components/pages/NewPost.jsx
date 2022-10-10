c0a1338d58ace66c07222b57232b33f9e0067b40
Adrian Rojo <rojoadrian16@gmail.com>
2022-10-10 15:46:08 -0500

    formatted cloudinary code with updated newPost page

--- src/components/pages/NewPost.jsx
+++ src/components/pages/NewPost.jsx
@@ -1,16 +1,67 @@
 import { useEffect, useState } from "react"
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+import {Image} from 'cloudinary-react'
 
 export default function NewPost({ currentUser, setCurrentUser }){
     const [posts, setPosts] = useState([])
     const [content, setContent] = useState("")
     const [errorMessage, setErrorMessage] = useState("")
+    const [fileInputState, setFileInputState] = useState('')
+    const [selectedFile, setSelectedFile] = useState('')
+    const [previewSource, setPreviewSource] = useState('')
+    const [imageIds, setImagesIds] = useState()
 
     const navigate = useNavigate()
 
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0]
+        previewFile(file);
+        setSelectedFile(file)
+    }
+
+
+    const previewFile = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file); //Converts the file to a url
+        reader.onloadend = () => { //Once the reader is done loading
+            setPreviewSource(reader.result);
+
+        }
+    }
+    useEffect(() => {
+        loadImages()
+    }, [])
+    // console.log(previewSource, "PREVIEW SOURCE")
+    // console.log(selectedFile, "SELECTED FILE")
+    const uploadImage = async (base64EncodedImage) => {
+       
+        try {
+            // const stringifyImage = JSON.stringif({data: base64EncodedImage})
+        await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts`, 
+        {content, 
+            userId : currentUser.id, 
+            body: base64EncodedImage,
+            headers: {'Content-Type': 'application/json'}, })
+        
+        } catch (err) {
+            console.error(err);
+        }
+    
+    }
+    const loadImages = async() => {
+        try{
+            const res =await fetch(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts/api/images`)
+            const data = await res.json()
+            setImagesIds(data)
+        }catch(err){
+            console.log(err)
+        }
+    }
     const handleCreate = async (e) => {
 		e.preventDefault()
+        if(!previewSource) return;
+        uploadImage(previewSource);
 		try{
 			const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/posts`, {content, userId : currentUser.id})
 			setPosts([...posts, response.data])
@@ -24,16 +75,28 @@ export default function NewPost({ currentUser, setCurrentUser }){
     return(
         <div>
             <h1>New Post</h1>
+            {imageIds && imageIds.map((imageId, idx) => (
+                <Image
+                    key = {idx}
+                    cloudName ="sdfie0"
+                    publicId = {imageId}
+                    width = '300'
+                    crop = 'scale'
+                />
+            ))}
             <form>   
                 <label htmlFor="content">Content</label>
                 <input type="text" name="content" id="content" value={content} onChange={(e) => setContent(e.target.value)}/>
-        
-                {/* <input hidden type="text" name="user" id="user"/> */}
-                    {/* for when we add image upload functionality */}
-                {/* <label htmlFor="image_url">Image URL</label> */}
-                {/* <input type="file" name="image_url" id="image_url"/> */}
+
+                <label htmlFor="file">File</label>
+                <input type = "file" name = "image" id = "image" onChange={handleFileInputChange} value={fileInputState}/>
+               
                 <button type="submit" onClick={handleCreate}>Submit</button>
             </form>
+            {previewSource && ( //If previewSource is true, then show the image
+                <img src={previewSource} alt="User uploaded image" 
+                style={{height: '300px'}}/>
+            )}
         </div>
     )
 }
