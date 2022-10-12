266d6b6b2cdba7221e8a6adf2cad5e17e4f7d71b
OmgValicious <ziallshamrock@gmail.com>
2022-10-11 21:27:00 -0700

    FINALLY??

--- src/components/pages/Post.jsx
+++ src/components/pages/Post.jsx
@@ -127,10 +127,28 @@ export default function Post({ currentUser, setCurrentUser }){
     const renderComments = comments.map((comment) => {
         return (
             <div key={comment._id}>
-                <p>@{comment.user.username} {comment.content}</p>
+                <div className="row p-0 d-flex border-bottom border-2">
+                    <div className='col-md-9 d-flex p-0 align-items-start mt-1 ms-2'>
+                       <p><span className='fw-bold d-flex'>{comment.user.username}: </span>
+                         {comment.content}</p>
+                    </div>
+                    <div className='col-sm-2 d-flex align-items-center'>
+                        {comment.user.username === currentUser.username ?<div className='d-flex justify-content-end align-items-center p-0'> 
+                        <Link className='p-0 m-1 btnComment' to={`/posts/${postid}/comments/${comment._id}/edit`}>
+                        <button className="btnComment p-0 shadow-none"><i class="bi bi-pencil-square"></i></button>
+                        </Link> 
+                        <button className='btnComment p-0 shadow-none' onClick={() => deleteComment(comment._id)}><i class="bi bi-trash3-fill"></i></button> 
+                        </div>: <p></p>}
+                        {/* <button onClick={handleCommentLikes}>Like</button> */}
+                    </div>
+                        <div className="d-flex justify-content-end">
+                            <Moment fromNow>{comment.createdAt}</Moment>
+                        </div>
+                </div>
+                {/* <p>@{comment.user.username} {comment.content}</p>
                 <Moment fromNow>{comment.createdAt}</Moment>
                 {comment.user.username === currentUser.username ? <div> <Link to={`/posts/${postid}/comments/${comment._id}/edit`}><button>Edit</button></Link> <button onClick={() => deleteComment(comment._id)}>Delete</button> </div>: <p></p>}
-                {/* <button onClick={handleCommentLikes}>Like</button> */}
+                <button onClick={handleCommentLikes}>Like</button> */}
             </div>
         )
     })
@@ -139,51 +157,77 @@ export default function Post({ currentUser, setCurrentUser }){
         <div>
             <h1 className="postTitlePage my-3">Post</h1>
             <div className='container d-flex justify-content-center'>
-                <div className="card mb-3 postCard">
+                <div className="card mb-3 border postCard">
                     <div className="row g-0">
-                        <div className="col-md-6">
-                            <img src={post.photo} alt={post.id} className='rounded-start mw-100' height="auto"/>
+                         {/* Holds image for card */}
+                        <div className="col-md-6 align-items-center">
+                            <img src={post.photo} alt={post.id} className='rounded-start mw-100 postImage' height="auto"/>
                         </div>
                         <div className="col-md-6">
-                            <div className='card-header d-flex justify-content-between'>
+                            {/* Header + Like and edit buttons */}
+                            <div className='card-header d-flex justify-content-between postCardHeader'>
                                 <div>
                                     <a href={`/${user.username}`} className='postCardTitle'>
-                                        <h5 className="card-title d-flex justify-content-start">{user.username}</h5>
+                                        <h5 className="card-title d-flex justify-content-start mt-1 fw-bold">{user.username}</h5>
                                     </a>
                                 </div>
                                 <div>
-                                    <button onClick={handleLikes}>{like ? "Unlike" : "Like"}</button>
-                                    { curUser ? <Link to={`/posts/${post._id}/edit`}> <button> Edit</button>
+                                    { curUser ? <Link to={`/posts/${post._id}/edit`}> <button className='btn mt-0 shadow-none editHeaderBtn'> Edit</button>
                                     </Link>: <p></p>}
                                 </div>
-                                
                             </div>
 
-                            <div className=" row card-body commentsSection">
-                                <h1>@{user.username}</h1>
-                                <p className="card-text d-flex justify-content-start">{post.content}</p>
+                            <div className="row card-body d-flex justify-content-start commentsSection p-0 m-1">
+                                <div className='row d-inline-flex justify-content-start mb-3 p-0 m-auto'>
+                                    {/* <div className='col-5 mt-1 p-0 fw-bold'>
+                                        <h1 className='postCardTitle'>@{user.username}</h1>
+                                    </div> */}
+                                    <div className='col-7 me-0 p-0'>
+                                        <p className="card-text d-flex justify-content-start">{post.content}</p>
+                                    </div>
+                                </div>
                                 <div className='cardComments'>
                                     {/* Render the list of comments */}
                                     {renderComments}
                                     {/* Error messages if they occur */}
-                                    {errorMessage}
-                                    {commentErrorMessage}
+                                    {/* {errorMessage}
+                                    {commentErrorMessage} */}
                                 </div>
                             </div>
 
-                            <div className='card-body d-flex justify-content-center'>
-                                <p>{likes} likes</p>
-                                <p className="card-text"><Moment fromNow>{post.createdAt}</Moment></p>
+                            <div className='card-body d-flex p-0 m-2 justify-content-between align-items-center cardPostBody'>
+                                <div className='d-flex align-items-top'>
+                                    <button onClick={handleLikes} type='button' className='mx-2 p-0 shadow-none likeBtnPost'>
+                                    {like ? "❤️" : "🤍"}</button>
+                                    
+                                    <p className='mt-2'>{likes} likes</p>
+                                </div>
+                                <p className="card-text mt-2"><Moment fromNow>{post.createdAt}</Moment></p>
                             </div>
 
-                            <div className='card-footer'>
-                                {currentUser?
-                                    <form onSubmit={handleComment}>
-                                    <label htmlFor="comment">@{currentUser.username}</label>
-                                    <input type="text" placeholder='add comment...' value={comment} onChange={(e) => setComment(e.target.value)}/>
-                                    <button type="submit" style = {{backgroundColor: '#FC6767', width: '90px' }}>Submit</button>
-                                    </form> 
-                                : <p></p>}
+                            <div className='card-footer d-flex align-items-start mt-1 p-0'>
+                                <div className="d-flex cardSubComment p-0">
+                                    {currentUser?
+                                        <form onSubmit={handleComment} className='m-2 p-0 d-flex justify-content-start'>
+                                        <div className='row d-flex '>
+                                            <div className='col'>
+                                                <div className="d-flex ms-2 p-0">
+                                                    <label htmlFor="comment">
+                                                    <i class="bi bi-emoji-smile"></i> {currentUser.username}
+                                                    </label>  
+                                                </div>
+                                                
+                                        
+                                                <input className='p-0 inputBarPost  shadow-none border m-1 align-items center' type="text" placeholder=' add comment...' value={comment} onChange={(e) => setComment(e.target.value)}/>
+                                                <button type="submit" style = {{backgroundColor: '#d86e03', width: '100px' }} className=' shadow-none p-0' >Submit</button>
+
+                                            </div>
+                                           
+                                        </div>
+                                        
+                                        </form> 
+                                    : <p></p>} 
+                                </div>
                             </div>
                         </div>
                     </div>
